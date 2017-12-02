920c36a200227f7ed2bf2504baaea4fbec589c09
gdsrosa <gabrieldsrosa@gmail.com>
2017-12-02 00:42:39 -0200

    Add configs to sync the database with the application and add the animals route

--- projeto_ong/ong_system/settings.py
+++ projeto_ong/ong_system/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ong_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ong',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
